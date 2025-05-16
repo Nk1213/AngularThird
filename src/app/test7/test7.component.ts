0f061071936c4c3fc7ea3a20746b5ef88a347aff
Naveen <navtec41@gmail.com>
2025-05-16 16:50:12 +0530

    r2

---
+++ src/app/test7/test7.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test7',
+  imports: [],
+  templateUrl: './test7.component.html',
+  styleUrl: './test7.component.css'
+})
+export class Test7Component {
+
+}
